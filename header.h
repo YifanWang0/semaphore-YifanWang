b45dcea75e682ba83d3f5bfc23665997449d10f7
YifanWang0 <ywang00@stuy.edu>
2020-01-06 01:54:47 -0500

    uncommented union struct

--- header.h
+++ header.h
@@ -15,14 +15,12 @@
 #define SEMKEY 24602
 #define SIZE 1000
 
-/**
 union semun {
     int val;		        // Value for SETVAL
 	struct semid_ds *buf;   //Buffer for IPC_STAT, IPC_SET
 	unsigned short *array;	//Array for GETALL, SETALL
 	struct seminfo *__buf;	//Buffer for IPC_INFO (Linux-specific)
 };
-**/
 
 void run();
 int creating();
